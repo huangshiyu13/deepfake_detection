ffa48837801524843a626fbedd732df6960f5657
huangsy13 <huangsy1314@163.com>
2020-05-05 19:04:06 +0800

    u

---
+++ timm/models/__init__.py
@@ -0,0 +1,25 @@
+from .inception_v4 import *
+from .inception_resnet_v2 import *
+from .densenet import *
+from .resnet import *
+from .dpn import *
+from .senet import *
+from .xception import *
+from .nasnet import *
+from .pnasnet import *
+from .selecsls import *
+from .efficientnet import *
+from .mobilenetv3 import *
+from .inception_v3 import *
+from .gluon_resnet import *
+from .gluon_xception import *
+from .res2net import *
+from .dla import *
+from .hrnet import *
+from .sknet import *
+
+from .registry import *
+from .factory import create_deepfake_model
+from .helpers import load_checkpoint, resume_checkpoint
+from .layers import TestTimePoolHead, apply_test_time_pool
+from .layers import convert_splitbn_model
---
+++ timm/models/factory.py
@@ -0,0 +1,66 @@
+from .registry import is_model, is_model_in_modules, model_entrypoint
+from .helpers import load_checkpoint
+
+
+def create_deepfake_model(
+        model_name,
+        pretrained=False,
+        num_classes=2,
+        in_chans=3,
+        checkpoint_path='',
+        ignore_keys=None,
+        strict=False,
+        **kwargs):
+    """Create a model
+
+    Args:
+        model_name (str): name of model to instantiate
+        pretrained (bool): load pretrained ImageNet-1k weights if true
+        num_classes (int): number of classes for final fully connected layer (default: 1000)
+        in_chans (int): number of input channels / colors (default: 3)
+        checkpoint_path (str): path of checkpoint to load after model is initialized
+
+    Keyword Args:
+        drop_rate (float): dropout rate for training (default: 0.0)
+        global_pool (str): global pool type (default: 'avg')
+        **: other kwargs are model specific
+    """
+    assert model_name == 'efficientnet_deepfake', '{} != efficientnet_deepfake'.format(model_name)
+
+    margs = dict(pretrained=pretrained, num_classes=num_classes, in_chans=in_chans)
+
+    # Only EfficientNet and MobileNetV3 models have support for batchnorm params or drop_connect_rate passed as args
+
+    is_efficientnet = is_model_in_modules(model_name, ['efficientnet', 'mobilenetv3'])
+    assert is_efficientnet
+
+    if not is_efficientnet:
+        kwargs.pop('bn_tf', None)
+        kwargs.pop('bn_momentum', None)
+        kwargs.pop('bn_eps', None)
+
+    # Parameters that aren't supported by all models should default to None in command line args,
+    # remove them if they are present and not set so that non-supporting models don't break.
+    if kwargs.get('drop_block_rate', None) is None:
+        kwargs.pop('drop_block_rate', None)
+
+    # handle backwards compat with drop_connect -> drop_path change
+    drop_connect_rate = kwargs.pop('drop_connect_rate', None)
+    if drop_connect_rate is not None and kwargs.get('drop_path_rate', None) is None:
+        print("WARNING: 'drop_connect' as an argument is deprecated, please use 'drop_path'."
+              " Setting drop_path to %f." % drop_connect_rate)
+        kwargs['drop_path_rate'] = drop_connect_rate
+
+    if kwargs.get('drop_path_rate', None) is None:
+        kwargs.pop('drop_path_rate', None)
+
+    if is_model(model_name):
+        create_fn = model_entrypoint(model_name)
+        model = create_fn(**margs, **kwargs)
+    else:
+        raise RuntimeError('Unknown model (%s)' % model_name)
+
+    if checkpoint_path:
+        load_checkpoint(model, checkpoint_path, strict=strict, ignore_keys=ignore_keys)
+
+    return model
